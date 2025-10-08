c48f76249256fb50302d227221d2e2701780521e
Harshjaiswal73 <155292983+Harshjaiswal73@users.noreply.github.com>
2025-10-08 22:35:18 +0000

    Arraylist

---
+++ String/ArrayList/Main.java
@@ -0,0 +1,38 @@
+package ArrayList;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        System.out.println(list + " ->" + list.size());
+
+        //add 
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        System.out.println(list + " ->"+list.size());
+
+        list.add(1,1000);
+        System.out.println(list +" -> "+ list.size());
+
+        int val  = list.get(1); // not list[1]
+        System.out.println(val);
+
+        list.set(1,2000); // not list[1] = 2000;
+        System.out.println(list + " -> "+list.size());
+
+        list.remove(1);
+        System.out.println(list + " ->" +list.size());
+
+        // for(int i=0;i<list.size();i++){
+        //     int val1 = list.get(i);
+        //     System.out.println(val1);
+        // }
+
+        for(int val1:list){
+            System.out.println(val1);
+        }
+    }
+}
