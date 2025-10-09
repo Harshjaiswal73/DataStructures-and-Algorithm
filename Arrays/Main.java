b231dd7732db567e4240a189b16bdb6b316d84b3
Harshjaiswal73 <155292983+Harshjaiswal73@users.noreply.github.com>
2025-10-09 19:24:06 +0000

    Array

---
+++ Arrays/Main.java
@@ -0,0 +1,19 @@
+package Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int []arr = new int[5];
+        arr[0]=44;
+        arr[1]=33;
+        arr[2]=45;
+        arr[3]=65;
+        arr[4]=75;
+
+        //System.out.println(arr.length);
+        for(int i=0;i<=arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+    
+}
